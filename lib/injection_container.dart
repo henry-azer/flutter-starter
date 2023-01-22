67e91064842afc4c9c4602fbdcd531493fab6c90
Henry-Azer <henryazer@outlook.com>
2023-01-23 00:29:48 +0200

    refactor injection container dependencies

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,66 +3,70 @@ import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/api/api_consumer.dart';
+import 'core/api/app_authentication.dart';
 import 'core/api/app_interceptors.dart';
 import 'core/api/dio_consumer.dart';
-import 'core/network/netwok_info.dart';
-import 'features/random_quote/data/datasources/random_quote_local_data_source.dart';
-import 'features/random_quote/data/datasources/random_quote_remote_data_source.dart';
-import 'features/random_quote/data/repositories/quote_repository_impl.dart';
-import 'features/random_quote/domain/repositories/quote_repository.dart';
-import 'features/random_quote/domain/usecases/get_random_quote.dart';
-import 'features/random_quote/presentation/cubit/random_quote_cubit.dart';
-import 'features/splash/data/datasources/lang_local_data_source.dart';
-import 'features/splash/data/repositories/lang_repository_impl.dart';
-import 'features/splash/domain/repositories/lang_repository.dart';
-import 'features/splash/domain/usecases/change_lang.dart';
-import 'features/splash/domain/usecases/get_saved_lang.dart';
-import 'features/splash/presentation/cubit/locale_cubit.dart';
+import 'core/network/network_info.dart';
+import 'data/datasources/authentication/authentication_local_datasource.dart';
+import 'data/datasources/authentication/authentication_remote_datasource.dart';
+import 'data/datasources/user/user_local_datasource.dart';
+import 'data/datasources/user/user_remote_datasource.dart';
+import 'data/repositories/authentication/authentication_repository.dart';
+import 'data/repositories/authentication/authentication_repository_impl.dart';
+import 'data/repositories/user/user_repository.dart';
+import 'data/repositories/user/user_repository_impl.dart';
+import 'data/datasources/localization/localization_local_data_source.dart';
+import 'data/repositories/localization/localization_repository_impl.dart';
+import 'data/repositories/localization/localization_repository.dart';
+import 'features/splash/domain/usecases/change_lang_usecase.dart';
+import 'features/splash/domain/usecases/get_saved_lang_usecase.dart';
+import 'features/splash/presentation/cubit/localization_cubit.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //! Features
 
-  // Blocs
-  sl.registerFactory<RandomQuoteCubit>(
-      () => RandomQuoteCubit(getRandomQuoteUseCase: sl()));
-  sl.registerFactory<LocaleCubit>(
-      () => LocaleCubit(getSavedLangUseCase: sl(), changeLangUseCase: sl()));
+  // !---- Cubits ----!
+  // splash
+  sl.registerFactory<LocalizationCubit>(() => LocalizationCubit(getSavedLangUseCase: sl(), changeLangUseCase: sl()));
 
-  // Use cases
-  sl.registerLazySingleton<GetRandomQuote>(
-      () => GetRandomQuote(quoteRepository: sl()));
-  sl.registerLazySingleton<GetSavedLangUseCase>(
-      () => GetSavedLangUseCase(langRepository: sl()));
-  sl.registerLazySingleton<ChangeLangUseCase>(
-      () => ChangeLangUseCase(langRepository: sl()));
 
-  // Repository
-  sl.registerLazySingleton<QuoteRepository>(() => QuoteRepositoryImpl(
-      networkInfo: sl(),
-      randomQuoteRemoteDataSource: sl(),
-      randomQuoteLocalDataSource: sl()));
-  sl.registerLazySingleton<LangRepository>(
-      () => LangRepositoryImpl(langLocalDataSource: sl()));
+  // !---- Use cases ----!
+  // splash
+  sl.registerLazySingleton<ChangeLangUseCase>(() => ChangeLangUseCase(langRepository: sl()));
+  sl.registerLazySingleton<GetSavedLangUseCase>(() => GetSavedLangUseCase(langRepository: sl()));
 
-  // Data Sources
-  sl.registerLazySingleton<RandomQuoteLocalDataSource>(
-      () => RandomQuoteLocalDataSourceImpl(sharedPreferences: sl()));
-  sl.registerLazySingleton<RandomQuoteRemoteDataSource>(
-      () => RandomQuoteRemoteDataSourceImpl(apiConsumer: sl()));
-  sl.registerLazySingleton<LangLocalDataSource>(
-      () => LangLocalDataSourceImpl(sharedPreferences: sl()));
 
-  //! Core
-  sl.registerLazySingleton<NetworkInfo>(
-      () => NetworkInfoImpl(connectionChecker: sl()));
+  // !---- Repository ----!
+  // authentication
+  sl.registerLazySingleton<AuthenticationRepository>(() => AuthenticationRepositoryImpl(networkInfo: sl(), authenticationLocalDataSource: sl(), authenticationRemoteDataSource: sl(),));
+  // user
+  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(networkInfo: sl(), userLocalDataSource: sl(), userRemoteDataSource: sl(),));
+  // localization
+  sl.registerLazySingleton<LocalizationRepository>(() => LocalizationRepositoryImpl(localizationLocalDataSource: sl()));
+
+
+  // !---- Data Sources ----!
+  // authentication
+  sl.registerLazySingleton<AuthenticationLocalDataSource>(() => AuthenticationLocalDataSourceImpl(sharedPreferences: sl()));
+  sl.registerLazySingleton<AuthenticationRemoteDataSource>(() => AuthenticationRemoteDataSourceImpl(apiConsumer: sl()));
+  // user
+  sl.registerLazySingleton<UserLocalDataSource>(() => UserLocalDataSourceImpl(sharedPreferences: sl()));
+  sl.registerLazySingleton<UserRemoteDataSource>(() => UserRemoteDataSourceImpl(apiConsumer: sl()));
+  // localization
+  sl.registerLazySingleton<LocalizationLocalDataSource>(() => LocalizationLocalDataSourceImpl(sharedPreferences: sl()));
+
+
+  // !---- Core ----!
+  sl.registerLazySingleton<AppAuthentication>(() => AppAuthenticationImpl(sharedPreferences: sl()));
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(connectionChecker: sl()));
   sl.registerLazySingleton<ApiConsumer>(() => DioConsumer(client: sl()));
 
-  //! External
+
+  // !---- External ----!
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
-  sl.registerLazySingleton(() => AppIntercepters());
+  sl.registerLazySingleton(() => AppInterceptors(sl()));
   sl.registerLazySingleton(() => LogInterceptor(
       request: true,
       requestBody: true,

a9920afe253a2b0e084f29af861d836506976956
Henry-Azer <henryazer@outlook.com>
2023-01-23 00:30:00 +0200

    refactor bloc observer

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -5,49 +5,36 @@ class AppBlocObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    if (kDebugMode) {
-      print('onCreate -- ${bloc.runtimeType}');
-    }
+    if (kDebugMode) print('onCreate -- ${bloc.runtimeType}');
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-
-    if (kDebugMode) {
-      print('onEvent -- ${bloc.runtimeType}, $event');
-    }
+    if (kDebugMode) print('onEvent -- ${bloc.runtimeType}, $event');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    if (kDebugMode) {
-      print('onChange -- ${bloc.runtimeType}, $change');
-    }
+    if (kDebugMode) print('onChange -- ${bloc.runtimeType}, $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    if (kDebugMode) {
-      print('onTransition -- ${bloc.runtimeType}, $transition');
-    }
+    if (kDebugMode) print('onTransition -- ${bloc.runtimeType}, $transition');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    if (kDebugMode) {
-      print('onError -- ${bloc.runtimeType}, $error');
-    }
+    if (kDebugMode) print('onError -- ${bloc.runtimeType}, $error');
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    if (kDebugMode) {
-      print('onClose -- ${bloc.runtimeType}');
-    }
+    if (kDebugMode) print('onClose -- ${bloc.runtimeType}');
   }
 }

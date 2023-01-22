0151c4d7550a51deb96f5118655400b4440658bc
Henry-Azer <henryazer@outlook.com>
2023-01-23 00:30:22 +0200

    refactor base app

--- lib/app.dart
+++ lib/app.dart
@@ -4,19 +4,19 @@ import 'config/locale/app_localizations_setup.dart';
 import 'config/routes/app_routes.dart';
 import 'config/themes/app_theme.dart';
 import 'core/utils/app_strings.dart';
-import 'features/splash/presentation/cubit/locale_cubit.dart';
+import 'features/splash/presentation/cubit/localization_cubit.dart';
 import 'injection_container.dart' as di;
 
-class QuoteApp extends StatelessWidget {
-  const QuoteApp({Key? key}) : super(key: key);
+class FlutterStarterApp extends StatelessWidget {
+  const FlutterStarterApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
         providers: [
-          BlocProvider(create: (context) => di.sl<LocaleCubit>()),
+          BlocProvider(create: (context) => di.sl<LocalizationCubit>()),
         ],
-        child: BlocBuilder<LocaleCubit, LocaleState>(
+        child: BlocBuilder<LocalizationCubit, LocalizationState>(
           buildWhen: (previousState, currentState) {
             return previousState != currentState;
           },
@@ -29,9 +29,9 @@ class QuoteApp extends StatelessWidget {
               onGenerateRoute: AppRoutes.onGenerateRoute,
               supportedLocales: AppLocalizationsSetup.supportedLocales,
               localeResolutionCallback:
-                  AppLocalizationsSetup.localeResolutionCallback,
+              AppLocalizationsSetup.localeResolutionCallback,
               localizationsDelegates:
-                  AppLocalizationsSetup.localizationsDelegates,
+              AppLocalizationsSetup.localizationsDelegates,
             );
           },
         ));
